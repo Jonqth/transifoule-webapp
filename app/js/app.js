6d7a1f8ecf38712f93d454bd9917be5a71c24844
Aymeric Gallissot <hello@aymericgallissot.fr>
2012-06-09 21:33:15 +0200

    Initial commit

---
+++ app/js/app.js
@@ -0,0 +1,16 @@
+/*
+ * Transifoule - 1.0
+ *
+ * Aymeric Gallissot
+ * http://aymericgallissot.fr
+ *
+ *
+ */
+
+$(function(){
+
+	$("body").hammer().bind("swipe", function(ev) {
+	        console.log(ev);
+	 });
+	
+});
